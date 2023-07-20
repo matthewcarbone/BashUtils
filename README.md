caddba1cf0a7ccba1f002344b9aa35a0c248fa8d
Matthew Carbone <x94carbone@gmail.com>
2023-07-20 14:15:42 -0400

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# BashUtils
-Simple quality of life utilities for the command line
+# Autojob
+
+A collection of bash scripts for making life on HPC just a bit easier. All scripts are contained in the `scripts` directory. They can be run from anywhere on your computer, but it is recommended you install them to your `PATH`.
+
+## Slurm Wrangler
+
+The `slurm_wrangler.sh` script was designed to solve a personal problem of mine. What happens if your SLURM job controller only allows you to submit `MAXJOBS` jobs at a time? For me, this is about 40, and when I have thousands of jobs I need to queue, this can become a problem.
+
+You might ask, but why not modify your code so that you don't need to submit thousands of jobs? I'm lazy, ok?
+
+So, `slurm_wrangler.sh` fixes this problem. It does the following:
+
+1. Installs itself into your `crontab`, such that it will, by default, run once/minute.
+2. Every time the cron executes, it checks the directory you specified recursively for jobs that match `submit.sbatch` by default.
+3. It checks the SLURM job controller for how many jobs you have queued under your username.
+4. It subtracts the provided `MAXJOBS` from the number of jobs you have queued or running, and will attempt to execute only that number of jobs.
+5. Every time a job is submitted, a file `QUEUED` is created in the directory where `submit.sbatch` was, such that it will not be resubmitted.
+6. Once all jobs are queued, the script will remove itself from your crontab.
+
+Note that in order for this to work, you need to have a `submit.sbatch` in every directory, setup in such a way that you `cd` to that directory, and run `sbatch submit.sbatch`.
+
+### Usage
+
+Using `slurm_wrangler.sh` is easy!
+
+```bash
+bash slurm_wrangler.sh
+    [--user=<STR>]
+    [--directory=<STR>]
+    [--maxjobs=<INT>]
+    [--cron=<STR>, "* * * * *"]
+```
+
+You must set your username, the directory you want to check recursively for `submit.sbatch` files, and the number of maximum jobs you want queued or running at once. Setting `cron` is optional, and defaults to `"* * * * *"` (once/min).
---
+++ scripts/slurm_wrangler.sh
@@ -0,0 +1,226 @@
+#!/bin/bash -l
+
+# Put in /usr/local/bin or somewhere else in your path!
+# This script must be accessible via `which`.
+
+# Example to add to crontab
+# crontab -l > mycron
+# echo "00 09 * * 1-5 echo hello" >> mycron
+# crontab mycron
+# rm mycron
+
+
+CRON="* * * * *"
+MAXJOBS=0
+USER="USER_UNSET"
+DIRECTORY="DIRECTORY_UNSET"
+TARGET_FILE="submit.sbatch"
+MODE="SETUP"
+QUEUED=".QUEUED"
+
+optspec=":hc-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            case "${OPTARG}" in
+                user=*)
+                    USER=${OPTARG#*=}
+                    ;;
+                directory=*)
+                    DIRECTORY=${OPTARG#*=}
+                    DIRECTORY=$(readlink -f "$DIRECTORY")
+                    ;;
+                maxjobs=*)
+                    MAXJOBS=${OPTARG#*=}
+                    ;;
+                cron=*)
+                    CRON=${OPTARG#*=}
+                    ;;
+                *)
+                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                        echo "Unknown option --${OPTARG}"
+                    fi
+                    ;;
+            esac;;
+        c)
+            echo "Running cronjob $NOW"
+            MODE="CRON"
+            ;;
+        h)
+            echo "usage: $0 [-v] [--loglevel[=]<value>]"
+            exit 2
+            ;;
+        *)
+            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+                echo "Non-option argument: '-${OPTARG}'"
+            fi
+            ;;
+    esac
+done
+
+
+function assert_args {
+    exitcode=0
+    if [ "$USER" = "USER_UNSET" ]; then
+        echo "error: user argument is required (--user=your_username)"
+        exitcode=1
+    fi
+    if [ "$DIRECTORY" = "DIRECTORY_UNSET" ]; then
+        echo "error: directory argument is required (--directory=your_directory)"
+        exitcode=1
+    fi
+    if [ "$exitcode" -eq 1 ]; then
+        exit 1
+    fi
+}
+
+function print_args {
+    echo "// SETTING PARAMETERS //"
+    echo "USER         $USER"
+    echo "DIRECTORY    $DIRECTORY"
+    echo "MAXJOBS      $MAXJOBS"
+    echo "TARGET FILE  $TARGET_FILE"
+    echo "CRON         $CRON"
+}
+
+
+function write_payload_to_crontab {
+    crontab -l > mycron
+    full_script_path=$(readlink -f "$0")
+    echo "$CRON bash $full_script_path -c --user=$USER --directory=$DIRECTORY --maxjobs=$MAXJOBS --targetfile=$TARGET_FILE >> $DIRECTORY/cronlog.log" >> mycron
+    crontab mycron
+    rm mycron
+}
+
+
+function remove_payload_from_crontab {
+    crontab -l > mycron
+    script_name=$(basename "$0")
+    sed -i "/$script_name/d" mycron
+    crontab mycron
+    rm mycron
+}
+
+
+function available_jobs {
+    queued_or_running_jobs=$(squeue -u "$USER" | awk 'NR!=1' | wc -l)
+    echo $((MAXJOBS - queued_or_running_jobs))
+}
+
+
+# Takes the file list as input
+function dryrun {
+    echo "DRYRUN - will attempt to submit the following $TARGET_FILE files"
+    for directory in $1; do
+        echo "$directory"
+    done
+}
+
+function remaining_jobs {
+    cctot=0
+    cc=0
+    for file in $1; do
+        parent="$(dirname "$file")"
+        cd "$parent" || exit
+        cctot=$((cctot+1))
+        if [ -e "$QUEUED" ]; then
+            cc=$((cc+1))
+        fi
+    done
+    remaining=$((cctot-cc))
+    echo "$remaining"
+}
+
+
+# Should be run every time on the cronjob
+# This searches for all target files and runs them if they haven't
+# been previously executed
+function execute {
+
+    echo "$NOW"
+
+    file_list=$(find "$DIRECTORY" -name "$TARGET_FILE" -exec echo {} \;)
+    
+    # Dryrun does not submit anything, just prints the directories
+    # This is run by default. The user has to set --maxjobs=<INT>
+    if [ "$MAXJOBS" == 0 ]; then
+        dryrun "$file_list"
+        remove_payload_from_crontab
+        exit 0
+    fi
+
+    # Get the remaining jobs
+    number_of_remaining_jobs=$(remaining_jobs file_list)
+
+    # If no jobs remain, stop
+    if [ "$number_of_remaining_jobs" -lt 1 ]; then
+        echo "Finished"
+        remove_payload_from_crontab
+        exit 0
+    fi
+
+    # Get the current job list
+    currently_available_jobs=$(available_jobs)
+
+    # If no jobs remain, stop
+    if [ "$currently_available_jobs" -lt 1 ]; then
+        echo "No queue space available"
+        exit 0
+    fi
+
+    # Actually execute
+    for file in $file_list; do
+
+        parent="$(dirname "$file")"
+        cd "$parent" || exit
+
+        # Break if we're done
+        if [ "$currently_available_jobs" -le 0 ]; then
+            break
+        fi
+
+        # If we already queued the job, we write that flag to the directory; we
+        # should check for this and if the QUEUED file exists, we continue since
+        # we don't want to submit the job multiple times
+        if [ -e "$QUEUED" ]; then
+            continue
+        fi
+
+        # Submit the job
+        if [ -e "$TARGET_FILE" ]; then
+            slurm_out=$(sbatch "$TARGET_FILE")
+            if [[ "$slurm_out" =~ .*"Submitted batch job".* ]]; then
+                job_id=${slurm_out##* }
+                echo "submitted job id $job_id: $parent"
+                currently_available_jobs="$(available_jobs)"
+            else
+                echo "submit error: $slurm_out"
+                continue
+            fi
+        elif [ -e "submit.sh" ]; then
+            bash submit.sh
+        else
+            echo "Unknown error"
+            exit 1
+        fi
+
+        # Add the QUEUED marker so that the script knows to skip that
+        # directory later
+        touch "$QUEUED"
+
+    done
+
+    echo "jobs remaining: $number_of_remaining_jobs"
+    echo
+}
+
+# main
+if [ "$MODE" = "CRON" ]; then
+    execute
+elif [ "$MODE" = "SETUP" ]; then
+    assert_args
+    print_args
+    write_payload_to_crontab
+else
+    exit 1
+fi
